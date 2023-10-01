441825ea56bb35652c2fce30031013d182cc0e1c
Kshitiz Kumar <kshitijk83@gmail.com>
2023-10-01 20:04:41 +0530

    deploy webhook fixes

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,6 +6,7 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  console.log("webhook called");
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
